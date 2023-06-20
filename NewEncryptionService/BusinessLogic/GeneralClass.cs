755e340a38840954b847d8c5560495b9a52b8c2d
Tosin Raphael <tosin.olowogboye@sterling.ng>
2023-06-20 17:06:04 +0100

    Add project files.

---
+++ NewEncryptionService/BusinessLogic/GeneralClass.cs
@@ -0,0 +1,121 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace NewEncryptionService.BusinessLogic
+{
+    public class GeneralClass
+    {
+        public string EncryptString(string key, string plainText)
+
+        {
+
+            byte[] iv = new byte[16];
+
+            Console.WriteLine("IV is:", iv);
+
+            byte[] array;
+
+            Console.WriteLine(Convert.ToBase64String(iv));
+
+
+
+            using (Aes aes = Aes.Create())
+
+            {
+
+                aes.Key = Encoding.UTF8.GetBytes(key);
+
+                aes.IV = iv;
+
+                aes.Mode = CipherMode.CBC;
+
+                aes.Padding = PaddingMode.PKCS7;
+
+
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+
+
+                using (MemoryStream memoryStream = new MemoryStream())
+
+                {
+
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+
+                    {
+
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+
+                        {
+
+                            streamWriter.Write(plainText);
+
+                            streamWriter.Flush();
+
+                        }
+
+
+
+                        array = memoryStream.ToArray();
+
+                    }
+
+                }
+
+            }
+
+
+
+            return Convert.ToBase64String(array);
+
+        }
+
+
+
+        public  string DecryptString(string key, string cipherText)
+         {
+
+            byte[] iv = new byte[16];
+
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+
+
+            using (Aes aes = Aes.Create())
+
+            {
+
+                aes.Key = Encoding.UTF8.GetBytes(key);
+
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+
+                {
+
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+
+                    {
+
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+
+                        {
+
+                            return streamReader.ReadToEnd();
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ NewEncryptionService/Controllers/UtilityController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewEncryptionService.BusinessLogic;
+
+namespace NewEncryptionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UtilityController : ControllerBase
+    {
+        protected GeneralClass _generalClass;
+
+        public UtilityController(GeneralClass generalClass)
+        {
+            _generalClass = generalClass;
+        }
+
+        [HttpGet("Encrypt/{key}/{jsontext}")]
+        public ActionResult Encrypt(string key, string jsontext)
+        {
+            return Ok(_generalClass.EncryptString(key, jsontext));
+        }
+
+        [HttpGet("Decrypt/{key}/{Encryptedpayload}")]
+        public ActionResult Decrypt(string key, string Encryptedpayload)
+        {
+            return Ok(_generalClass.DecryptString(key, Encryptedpayload));
+        }
+    }
+}
